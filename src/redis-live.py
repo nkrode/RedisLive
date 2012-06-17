062af0c9a924a7c28445f0cadfb2ea02146b723e
Nitin Kumar <heihaozi2006@163.com>
2012-06-16 21:12:04 -0700

    clean up

--- src/redis-live.py
+++ src/redis-live.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 import tornado.ioloop
 import tornado.web
 
--- src/util/redis-fill-data.py
+++ src/util/redis-fill-data.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python
+
 import redis
 from time import strftime
 import time
@@ -8,18 +10,43 @@ def monitor():
 	redisPort = 6379
 	redisClient = redis.StrictRedis(host=redisHost, port=redisPort, db=0)	
 	
-	while True:
-		x = random.randint(1, 10)
-		y = random.randint(1, 100)
+	for x in xrange(1,10000):	
 
-		if x==1:
-			redisClient.set("string:" + str(y) + ":name","nitin")
-		elif x==2:
-			redisClient.get("string:" + str(y) + ":name")
-		elif x==4:
-			redisClient.hset("hash:" + str(y), str(y), str(y))
-		else:
-			redisClient.setex("string:" + str(y),y,str(y))
+		x = random.randint(1, 100)
+		y = random.randint(1, 20)
+
+		if y==1: 
+			for z in xrange(1,x):
+ 				redisClient.set("Key:" + `x`, x)
+		elif y==2: 
+			for z in xrange(1,x):
+				redisClient.get("Key:" + `x`)
+		elif y==4:
+			for z in xrange(1,x):
+				redisClient.hset("HashKey:" + `x`, x, x)
+		elif y==5:
+			for z in xrange(1,(x/2)+2):
+				redisClient.setex("Key:" + `x`, 1000, x)
+		elif y==6:
+			for z in xrange(1,x):
+				redisClient.hexists("HashKey:" + `x`, y)
+		elif y==7:
+			for z in xrange(1,x):
+				redisClient.setbit("BitSet:" + `x`, 1, 1)
+		elif y==8:
+			for z in xrange(1,x):
+				redisClient.getbit("BitSet:" + `x`, 1)
+		elif y==9:
+			for z in xrange(1,x):
+				redisClient.expire("Key:"+ `x`, 2000)
+		elif y==11:
+			redisClient.flushall()
+			
+			
+
+		time.sleep(0.1)
+
+		
 		
 
 
