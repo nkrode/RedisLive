b8f37709887ea0df713f68c926d9fb48fbb8f227
Nitin Kumar <heihaozi2006@163.com>
2012-06-19 00:08:52 -0700

    Added support to switch Data store from sqlite to redis via DataStoreType property in redis-live.conf, defaults to redis

--- src/api/util/settings.py
+++ src/api/util/settings.py
@@ -16,3 +16,8 @@ class RedisLiveSettings(object):
 	def GetSettings():
 		config = open("redis-live.conf")
 		return json.loads(config.read())
+
+	@staticmethod
+	def GetDataStoreType():
+		config = RedisLiveSettings.GetSettings()
+		return config["DataStoreType"]
--- src/dataprovider/dataprovider.py
+++ src/dataprovider/dataprovider.py
@@ -1,8 +1,19 @@
-from sqliteprovider import RedisStatsProvider
+#from sqliteprovider import RedisStatsProvider
 #from redisprovider import RedisStatsProvider
+
+from api.util.settings import RedisLiveSettings
+import sqliteprovider
+import redisprovider
+
 
 class RedisLiveDataProvider(object):
 
 	@staticmethod
 	def GetProvider():
-		return RedisStatsProvider()
+		dataStoreType = RedisLiveSettings.GetDataStoreType()
+
+		if dataStoreType=="redis":
+			return redisprovider.RedisStatsProvider()
+		else:
+			return sqliteprovider.RedisStatsProvider()			
+			
