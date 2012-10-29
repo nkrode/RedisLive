f328e539c8345a2972a1921d7ee7751560914743
Nitin Kumar <kumarnitin@live.com>
2012-10-29 11:08:34 -0700

    added support for tornado's command line options

--- src/redis-live.py
+++ src/redis-live.py
@@ -2,6 +2,7 @@
 
 import tornado.ioloop
 import tornado.options
+from tornado.options import define, options
 import tornado.web
 
 from api.controller.BaseStaticFileHandler import BaseStaticFileHandler
@@ -13,20 +14,23 @@ from api.controller.CommandsController import CommandsController
 from api.controller.TopCommandsController import TopCommandsController
 from api.controller.TopKeysController import TopKeysController
 
-
-# Bootup
-application = tornado.web.Application([
-  (r"/api/servers", ServerListController),
-  (r"/api/info", InfoController),
-  (r"/api/memory", MemoryController),
-  (r"/api/commands", CommandsController),
-  (r"/api/topcommands", TopCommandsController),
-  (r"/api/topkeys", TopKeysController),
-  (r"/(.*)", BaseStaticFileHandler, {"path": "www"})
-], debug="True")
-
-
 if __name__ == "__main__":
-	tornado.options.parse_command_line()
-	application.listen(8888)
-	tornado.ioloop.IOLoop.instance().start()
+    define("port", default=8888, help="run on the given port", type=int)
+    define("debug", default=0, help="debug mode", type=int)
+    tornado.options.parse_command_line()
+
+    # Bootup
+    handlers = [
+    (r"/api/servers", ServerListController),
+    (r"/api/info", InfoController),
+    (r"/api/memory", MemoryController),
+    (r"/api/commands", CommandsController),
+    (r"/api/topcommands", TopCommandsController),
+    (r"/api/topkeys", TopKeysController),
+    (r"/(.*)", BaseStaticFileHandler, {"path": "www"})
+    ]
+
+    server_settings = {'debug': options.debug}
+    application = tornado.web.Application(handlers, **server_settings)
+    application.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
