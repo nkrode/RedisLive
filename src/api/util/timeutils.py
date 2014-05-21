2c90faf766e00372d54e3c4a1962680a47a7b941
evilNullPointer <jeremie.charest@frimastudio.com>
2014-05-21 15:43:58 -0400

    Fix #63 for python 2.6 support on datetime.total_seconds method
    I quickly found a fix to this bug in the Mozilla github :
    https://github.com/mozilla/mozdownload/issues/73

--- src/api/util/timeutils.py
+++ src/api/util/timeutils.py
@@ -2,5 +2,15 @@ from datetime import datetime
 
 def convert_to_epoch(timestamp):
 	diff = (timestamp - datetime(1970, 1, 1))
-	seconds = int(diff.total_seconds())
+	seconds = int(total_seconds(diff))
 	return seconds
+
+
+# Original fix for Py2.6: https://github.com/mozilla/mozdownload/issues/73
+def total_seconds(dt):
+	# Keep backward compatibility with Python 2.6 which doesn't have
+	# this method
+	if hasattr(datetime, 'total_seconds'):
+		return dt.total_seconds()
+	else:
+		return (dt.microseconds + (dt.seconds + dt.days * 24 * 3600) * 10**6) / 10**6
