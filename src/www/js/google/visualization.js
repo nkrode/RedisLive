8184d20be9cb6e05e43e19cf5ecd5e78f61b2422
zhangjiayin <zhangjiayin@gu360.com>
2015-08-12 12:32:43 +0800

    fix google script

---
+++ src/www/js/google/visualization.js
@@ -0,0 +1,10 @@
+if (window['google'] != undefined && window['google']['loader'] != undefined) {
+    if (!window['google']['visualization']) {
+        window['google']['visualization'] = {};
+        google.visualization.Version = '1.0';
+        google.visualization.JSHash = 'abbdd6ab2d343a51d49841bf93d022fb';
+        google.visualization.LoadArgs = 'file\75visualization\46v\0751\46packages\75corechart';
+    }
+ //   google.loader.writeLoadTag("css", google.loader.ServiceBase + "/api/visualization/1.0/abbdd6ab2d343a51d49841bf93d022fb/ui+en.css", false);
+ //   google.loader.writeLoadTag("script", google.loader.ServiceBase + "/api/visualization/1.0/abbdd6ab2d343a51d49841bf93d022fb/format+en,default+en,ui+en,corechart+en.I.js", false);
+}
