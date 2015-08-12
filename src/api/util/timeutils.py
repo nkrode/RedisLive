3aba768c7a3c11f2941db36d0292cd5810433596
zhangjiayin <zhangjiayin@gu360.com>
2015-08-12 12:59:40 +0800

    fix python2.7.9

--- src/api/util/timeutils.py
+++ src/api/util/timeutils.py
@@ -1,5 +1,4 @@
-from datetime import datetime
-
+import datetime
 def total_seconds(td):
     # Keep backward compatibility with Python 2.6 which doesn't have
     # this method
@@ -9,7 +8,10 @@ def total_seconds(td):
         return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**6
 
 def convert_to_epoch(timestamp):
-	diff = (timestamp - datetime(1970, 1, 1))
+        if (type(timestamp) is datetime.date):
+            timestamp = datetime.datetime.fromordinal(timestamp.toordinal())
+        timestamp = timestamp.replace(tzinfo=None)
+	diff = (timestamp - datetime.datetime(1970, 1, 1))
 	seconds = int(total_seconds(diff))
 	return seconds
 
@@ -18,7 +20,7 @@ def convert_to_epoch(timestamp):
 def total_seconds(dt):
 	# Keep backward compatibility with Python 2.6 which doesn't have
 	# this method
-	if hasattr(datetime, 'total_seconds'):
+	if hasattr(datetime.datetime, 'total_seconds'):
 		return dt.total_seconds()
 	else:
 		return (dt.microseconds + (dt.seconds + dt.days * 24 * 3600) * 10**6) / 10**6
